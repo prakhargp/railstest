ba90c005df2097de15099914f4dc9de66237e4d9
pgupta <pgupta@C02D1B0WMD6M.local>
2021-01-23 15:44:13 +0530

    Used case expression for grades

--- q3.rb
+++ q3.rb
@@ -20,9 +20,29 @@ class Students
         end
         
     end
+    def grades_by_switch(name_of_student)
+        @@arr.each do |hash|
+            if hash[:name] == name_of_student
+                case hash[:marks]
+                when 80..100
+                    return "Grade : A"
+                when 60..80
+                    return "Grade : B"
+                when 40..60
+                    return "Grade : C"
+                else
+                return  "Grade : D"
+                end       
+            else
+                return "Student not found"    
+            end    
+            break
+        end
+    end
 end
 
 new_student = Students.new
-puts new_student.student_details("harshit", 24, 34)
+puts new_student.student_details("harshit", 24, 20)
 puts new_student.passorfail("harshit")
+puts new_student.grades_by_switch("harshit")
 
