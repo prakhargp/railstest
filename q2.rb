8b0886e645a132332cc301c6e5ecccba29a07f26
pgupta <pgupta@C02D1B0WMD6M.local>
2021-01-23 11:57:48 +0530

    Changed the formula for Volume

--- q2.rb
+++ q2.rb
@@ -2,7 +2,7 @@ class Cylinder
     RADIUS = 10
     PI = 3.14 
     def volume(height)
-        volume_of_cylinder = 2 * PI * RADIUS * height 
+        volume_of_cylinder =  PI * (RADIUS ** 2) * height 
         return volume_of_cylinder
     end
 end
